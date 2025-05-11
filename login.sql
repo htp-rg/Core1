d9038c9e748fe54b1732ba8a132aaf04d4bdf115
www <ariaenajnasucag@gmaIl.com>
2025-05-11 23:32:17 +0800

    Create login.sql

---
+++ login.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS HR1;
+USE HR1;
+
+
+CREATE TABLE IF NOT EXISTS employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- 
+INSERT INTO employees (email, password) VALUES ('employee1@gmail.com', '12345');
+INSERT INTO users (email, password) VALUES ('admin@gmail.com', '12345');
